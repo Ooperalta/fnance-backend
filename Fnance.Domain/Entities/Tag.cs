471adfc63394d55fc8fa5a6f0c86ca5eeadc0a4b
Ooperalta <164176557+Ooperalta@users.noreply.github.com>
2026-04-16 22:38:39 +0100

    feat: add Tag entity class

---
+++ Fnance.Domain/Entities/Tag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fnance.Domain.Entities
+{
+    public class Tag
+    {
+        public Guid Id { get; private set; }
+
+        public Guid CategoryId { get; private set; }
+
+        public Guid UserId { get; private set; }
+        public string Name { get; private set; }
+
+        public Tag(Guid categoryId, Guid userId, string name)
+        {
+            Id = Guid.NewGuid();
+            CategoryId = categoryId;
+            UserId = userId;
+            Name = name;
+
+        }
+
+    }
+}
