d8299821a96d70a6ea9b6797c26e4a23d2429506
Ooperalta <164176557+Ooperalta@users.noreply.github.com>
2026-04-21 11:45:52 +0100

    feat: add ItransactionRepository

---
+++ Fnance.Domain/Repositories/ITransactionRepository.cs
@@ -0,0 +1,16 @@
+﻿using Fnance.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fnance.Domain.Repositories
+{
+    public interface ITransactionRepository
+    {
+        Task AddAsync(Transaction transaction);
+
+        Task<IEnumerable<Transaction>> GetAllByUserIdAsync(Guid userId);
+    }
+}
---
+++ Fnance.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Fnance.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fnance.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(User user);
+
+        Task<User> GetByEmailAsync(string email);
+
+    }
+}
